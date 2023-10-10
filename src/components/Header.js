7b3e264cf4315ef57692978c2c3339b26714df2d
Segcal <seguncaleb3@gmail.com>
2023-10-10 06:09:56 +0100

    main
    cool taskTracker

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import Button from "./Button"
+
+
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <>
+      <header className="header">
+        <h1>{title}</h1>
+          <Button onClick={onAdd} color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} />
+      </header>
+    </>
+  )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker'
+}
+
+export default Header
